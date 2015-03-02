849779b641c2a71551a8fde023c98786856b8600
Brian Kirkbride <brian@otherpeoplespixels.com>
2015-03-02 15:35:54 -0800

    Bump version to 0.1.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject curmudjeon "0.1.4"
+(defproject curmudjeon "0.1.5"
   :description "Render HTML using Facebook's React from Clojure on the JVM."
   :url "http://github.com/OtherPeoplesPixels/curmudjeon"
   :license {:name "Eclipse Public License"
